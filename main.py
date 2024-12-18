68b80ec452f605b9eabb8cb1961b4f81c2d2f0d1
iclalkahramanoglu <iclal.kahramanoglu@std.yeditepe.edu.tr>
2024-12-18 17:40:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+sayi1= float(input("birinci sayıyı girin"))
+sayi2= float(input("ikinci sayıyı girin"))
+sayi3= float(input("üçüncü sayıyı girin"))
+
+# sayıların toplamını hesaplayalım
+toplam = sayi1 + sayi2 + sayi3
+
+# sonucu ekrana yazdıralım 
+print(" üç sayının toplamı: ", toplam)
